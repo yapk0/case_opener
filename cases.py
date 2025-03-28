85a3b62ee4f990928021dee5a81b2149deea7498
yapk0 <daniel.pajak@onet.com.pl>
2025-03-28 18:54:27 +0100

    added realistic case

---
+++ cases.py
@@ -0,0 +1,31 @@
+'''
+skin drop rates:
+    Consumer Grade (Common Skins) - 79.92%
+    Industrial Grade (Uncommon Skins) - 15.98%
+    Mil-Spec (Rare Skins) - 3.2%
+    Restricted (Mythical Skins) - 0.64%
+    Classified (Legendary Skins) - 0.32%
+    Covert (Exceedingly Rare Skins) - 0.16%
+    Knives and Gloves (Ultra-Rare Items) - ~0.25% or lower (estimated)
+    '''
+
+kilowatt_case = [
+    {"name": "Rare Knife", "chance": 0.26},
+    {"name": "AK-47 Inheritance", "chance": 0.16},
+    {"name": "AWP Chrome Cannon", "chance": 0.16},
+    {"name": "USP-S Jawbreaker", "chance": 0.32},
+    {"name": "Zeus x27 Olympus", "chance": 0.32},
+    {"name": "M4A1-S Black Lotus", "chance": 0.32},
+    {"name": "Sawed-Off", "chance" : 0.64},
+    {"name": "MP7 Just Smile", "chance" : 0.64},
+    {"name": "Five-seveN Hybrid", "chance": 0.64},
+    {"name": "M4A4 Etch Lord", "chance":0.64},
+    {"name": "Glock-18 Block-18", "chance": 0.64},
+    {"name": "XM1014 Irezumi", "chance": 3.20},
+    {"name": "UMP-45 Motorized", "chance": 3.20},
+    {"name": "Tec-9 Slag", "chance": 3.20},
+    {"name": "SSG Dezastre", "chance": 3.20},
+    {"name": "Nova Dark Sigil", "chance": 3.20},
+    {"name": "MAC-10 Light Box", "chance":3.20},
+    {"name": "Dual Berettas Hideout", "chance":3.20}
+]
